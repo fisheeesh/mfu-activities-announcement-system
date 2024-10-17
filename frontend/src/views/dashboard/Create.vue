55699baee1cbcacad96e03deac1223c6d25644d1
fisheeesh <swanphyo444@gmail.com>
2024-10-18 00:55:52 +0700

    create activity start

--- frontend/src/views/dashboard/Create.vue
+++ frontend/src/views/dashboard/Create.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>Create</h1>
+    <div class="container">
+        <h3 class="fs-4 form-title fw-bold text-center mb-2">Create a new activity</h3>
+        <div class="row">
+            <div class="col-md-6">col 1</div>
+            <div class="col-md-6">col 2</div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +15,7 @@ export default {
 </script>
 
 <style>
-
+.form-title{
+    margin-top: 100px;
+}
 </style>
